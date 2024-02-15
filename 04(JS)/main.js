4f86a09ed60a05ae46fdb1cd66ace4d1f2da15f0
Alex36936 <vakulenkosasha1995@gmail.com>
2024-02-15 22:51:44 +0100

    homework 04(JS)

---
+++ 04(JS)/main.js
@@ -0,0 +1,75 @@
+//Створіть функцію wellcomUser, яка приймає масив і функцію-колбек hiUser яка виводить в console ('Вітаю ${user}'), 
+//і викликає цю функцію для кожного елемента масиву.
+//Task #1
+let wellcomeArr=['orange','lime','melone','pinaple']
+function wellcomUser(wellcomeArr) {
+    wellcomeArr.call
+    let hiUser = prompt();
+}
+console.logt{''Вітаю ${hiUser}''}
+
+
+
+
+//Task #3
+
+// let emptyArr = [];
+
+// emptyArr.push("Orange");
+// emptyArr.push("Lime");
+// emptyArr.push("some Fruits");
+
+// alert(emptyArr);
+
+// console.log(emptyArr);
+
+//Завдання 2: Створіть функцію, яка приймає масив і елемент, і використовує push для додавання цього елемента до масиву.
+//pop:
+// let newpopArr=['time', 'country', 'city',90];
+// function newPop(newpopArr) {
+//     for (i = 0; i < newpopArr.length; i++);
+//     newpopArr.push('day');
+// }
+// console.log(newpopArr);
+
+
+//Task #    
+
+// let fullArr = ['number', 'fruit', 34, 45, 'name'];
+// console.log(fullArr);
+// console.log(fullArr.length);
+
+// fullArr.pop(4);
+// console.log(fullArr);
+
+//Завдання 2: Напишіть функцію, яка приймає масив і 
+//використовує pop для видалення останнього елемента масиву.
+//unshift:
+
+
+// Task #
+
+// let newArr = ['room', 'appartment', 'house', 'building'];
+// console.log(newArr);
+
+// newArr.unshift('new item');
+// console.log(newArr);
+
+//Завдання 2: Напишіть функцію, яка приймає масив і елемент, 
+//і використовує unshift для додавання цього елемента в початок
+//масиву.
+
+
+//Task #
+
+
+
+// let newTask = ['room', 'appartment', 'house', 'building'];
+// console.log(newTask);
+
+// newTask.shift('new item');
+// console.log(newTask);
+
+
+//Завдання 2: Напишіть функцію, яка приймає масив і використовує shift
+//для видалення першого елемента масиву.
