902fd6624f60a41a9869560511234d8cd5089d30
Alex36936 <vakulenkosasha1995@gmail.com>
2024-02-16 00:44:06 +0100

    HW 04(JS)

--- 04(JS)/main.js
+++ 04(JS)/main.js
@@ -1,75 +1,103 @@
+
+
+//---------------------------------------------------
+//Task #push
+
+let emptyArr = [];
+emptyArr.push("Orange");
+emptyArr.push("Lime");
+emptyArr.push("some Fruits");
+alert(emptyArr);
+console.log(emptyArr);
+
+//--------------------------------
+
+let freeArr = [];
+let newfreeArr = [67, 78, 29, 45];
+function emptyArrey (array){
+  array.push('hard numbers');
+}
+emptyArrey(newfreeArr);
+console.log(newfreeArr);
+
+
+//---------------------------------------------------
+//Task #pop
+
+let fullArr = ['number', 'fruit', 34, 45, 'name'];
+console.log(fullArr);
+console.log(fullArr.length);
+fullArr.pop(4);
+console.log(fullArr);
+
+//--------------------------------
+
+let popArr = [];
+let newpopArr = [67, 78, 29, 45];
+function emptyArrey (array){
+  array.pop('hard numbers');
+}
+emptyArrey(newpopArr);
+console.log(newpopArr);
+
+
+//---------------------------------------------------
+// Task #unshift
+
+let newArr = ['room', 'appartment', 'house', 'building'];
+console.log(newArr);
+
+newArr.unshift('new item');
+console.log(newArr);
+
+//--------------------------------
+
+let unshiftArr = [];
+let newunshiftArr = ['car','bus','moto'];
+function emptyArrey (array){
+  array.unshift('some transport');
+}
+emptyArrey(newunshiftArr);
+console.log(newunshiftArr);
+
+//---------------------------------------------------
+//Task #shift
+
+
+let newTask = ['room', 'appartment', 'house', 'building'];
+console.log(newTask);
+
+newTask.shift('new item');
+console.log(newTask);
+
+//--------------------------------
+
+let counrtyArr = [];
+let newshiftArr = ['Ireland','Austria','Hungary'];
+function emptyArrey (array){
+  array.shift('Germany');
+}
+emptyArrey(newshiftArr);
+console.log(newshiftArr);
+
+//---------------------------------------------------
 //Створіть функцію wellcomUser, яка приймає масив і функцію-колбек hiUser яка виводить в console ('Вітаю ${user}'), 
 //і викликає цю функцію для кожного елемента масиву.
-//Task #1
-let wellcomeArr=['orange','lime','melone','pinaple']
-function wellcomUser(wellcomeArr) {
-    wellcomeArr.call
-    let hiUser = prompt();
+let userArr = ['Tom', 'Karl', 'Kalvin'];
+function hiUser(user) {
+    for (i = 0; i < userArr.length; i++);
+     console.log(`Вітаю ${user}`)
+ }
+function wellcomUser() {
 }
-console.logt{''Вітаю ${hiUser}''}
-
-
-
-
-//Task #3
-
-// let emptyArr = [];
-
-// emptyArr.push("Orange");
-// emptyArr.push("Lime");
-// emptyArr.push("some Fruits");
-
-// alert(emptyArr);
-
-// console.log(emptyArr);
-
-//Завдання 2: Створіть функцію, яка приймає масив і елемент, і використовує push для додавання цього елемента до масиву.
-//pop:
-// let newpopArr=['time', 'country', 'city',90];
-// function newPop(newpopArr) {
-//     for (i = 0; i < newpopArr.length; i++);
-//     newpopArr.push('day');
-// }
-// console.log(newpopArr);
-
-
-//Task #    
-
-// let fullArr = ['number', 'fruit', 34, 45, 'name'];
-// console.log(fullArr);
-// console.log(fullArr.length);
-
-// fullArr.pop(4);
-// console.log(fullArr);
-
-//Завдання 2: Напишіть функцію, яка приймає масив і 
-//використовує pop для видалення останнього елемента масиву.
-//unshift:
-
-
-// Task #
-
-// let newArr = ['room', 'appartment', 'house', 'building'];
-// console.log(newArr);
-
-// newArr.unshift('new item');
-// console.log(newArr);
-
-//Завдання 2: Напишіть функцію, яка приймає масив і елемент, 
-//і використовує unshift для додавання цього елемента в початок
-//масиву.
-
-
-//Task #
-
-
-
-// let newTask = ['room', 'appartment', 'house', 'building'];
-// console.log(newTask);
-
-// newTask.shift('new item');
-// console.log(newTask);
-
-
-//Завдання 2: Напишіть функцію, яка приймає масив і використовує shift
-//для видалення першого елемента масиву.
+hiUser(userArr);
+console.log(user);
+ //Додаткова робота:
+    //Створіть новий массив за допомогою методів розглянутих на уроці в якому видалені всі значення "Remove" з масиву -
+    //["Keep", "Remove", "Keep", "Remove", "Keep", "Keep", "Remove", "Remove"] в результаті повинен бути ось такий новий
+//массив ["Keep", "Keep", "Keep", ...]
+    
+let newremoveArr = ["Keep", "Remove", "Keep", "Remove", "Keep", "Keep", "Remove", "Remove"];
+console.log(newremoveArr);
+newremoveArr.filter("Remove");
+console.log(newremoveArr);
