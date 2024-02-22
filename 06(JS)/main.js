ac27017efd581d1c963250f8e7438b86481f39d7
Alex36936 <vakulenkosasha1995@gmail.com>
2024-02-22 01:30:09 +0100

    Home-work 06(JS)

---
+++ 06(JS)/main.js
@@ -0,0 +1,48 @@
+//Task #1-2-----------------------------------------
+
+class Person{
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    sayHello(){
+       console.log(`Hello User ${this._name}`);
+    }
+}
+let newName = new Person('John', 30);
+console.log(newName);
+newName.sayHello();
+
+
+//Task #3------------------------------------------
+
+class Student extends Person{
+    constructor(name, age, studentID) {
+        super(name, age);
+        this._studentID = studentID;
+    }
+    info() {
+        console.log(`About person: ${this._name}, ${this._age},${this._studentID}`);
+    }
+}
+let newStudent = new Student('John', 30, 'study');
+console.log(newStudent);
+newStudent.info();
+
+
+//Task #4------------------------------------------
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (item) {
+    numbers.reverse(item);    
+})
+
+console.log(numbers);
+
+
+//Task #5------------------------------------------
+
+let floatNumber = [15.8, 4.3, 22.9, 3.5];
+let simpleArrr = floatNumber.map(Math.round);
+console.log(simpleArrr);
+
